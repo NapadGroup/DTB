6cc7a198180d802cb74da6b233085a40d7b956a2
Gnkalk <74614163+Gnkalk@users.noreply.github.com>
2021-11-06 14:58:49 +0330

    Add installer, khode DTB

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+GREEN=`tput setaf 2`
+
+clear
+
+sudo rm /usr/share/DTB
+
+sudo mkdir /usr/share/DTB
+
+sudo cp -T -f ./src/DTB.py  /usr/share/DTB/DTB.py
+
+sudo cp -T -f dtb /usr/bin/dtb
+
+echo "${GREEN}Use dtb Command to Run DTB"
